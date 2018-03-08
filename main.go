f55338a24891f148fa1161c7ce9cb887dc4a91e5
Erik Agsjö <erik.agsjo@gmail.com>
2018-03-08 01:27:04 +0100

    First!

---
+++ main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"bufio"
+	"database/sql"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	dbFile := flag.String("db", "hej.db", "Database file")
+	init := flag.Bool("init", false, "Initialize database")
+	load := flag.String("load", "", "Load entries from file, line by line")
+	match := flag.Bool("match", false, "Read from stdin and match each line")
+
+	flag.Parse()
+
+	db, err := sql.Open("sqlite3", *dbFile)
+	if err != nil {
+		panic(err)
+	}
+
+	if *init {
+		_, err = db.Exec(`create virtual table stuff using fts5(txt, tokenize="unicode61");`)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if *load != "" {
+		wordFile, err := os.Open(*load)
+		if err != nil {
+			panic(err)
+		}
+		tx, err := db.Begin()
+		if err != nil {
+			panic(err)
+		}
+		st, err := tx.Prepare("insert into stuff (txt) values(?)")
+		if err != nil {
+			panic(err)
+		}
+		fileScanner := bufio.NewScanner(wordFile)
+		loaded := 0
+		for fileScanner.Scan() {
+			line := fileScanner.Text()
+			line = strings.TrimSpace(line)
+			if line != "" {
+				_, err = st.Exec(line)
+				if err != nil {
+					panic(err)
+				}
+				loaded++
+			}
+		}
+		err = tx.Commit()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Loaded %v items\n", loaded)
+	}
+
+	if *match {
+		rows, err := db.Query("select count(*) from stuff")
+		if err != nil {
+			panic(err)
+		}
+		var phrases int32
+		rows.Next()
+		err = rows.Scan(&phrases)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%v phrases loaded\n", phrases)
+
+		matchScanner := bufio.NewScanner(os.Stdin)
+		for matchScanner.Scan() {
+			line := matchScanner.Text()
+			start := time.Now()
+			rows, err := db.Query("select txt, rank from stuff where txt match ? order by rank", line)
+			t1 := time.Now()
+			if err != nil {
+				panic(err)
+			}
+			hits := 0
+			for rows.Next() {
+				var hit string
+				var rank float32
+				if err := rows.Scan(&hit, &rank); err != nil {
+					panic(err)
+				}
+				hits++
+				fmt.Printf("%q (%v)\n", hit, rank)
+			}
+			rows.Close()
+			t2 := time.Now()
+			dur1 := t1.Sub(start)
+			dur2 := t2.Sub(t1)
+
+			fmt.Printf("--- %v hits in (%v + %v) ---\n", hits, dur1, dur2)
+		}
+	}
+	_ = db.Close()
+}
