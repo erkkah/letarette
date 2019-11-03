6487547c1c86d41d9be8ce0ac7ec811630e3a225
Erik Agsjö <erik.agsjo@gmail.com>
2019-11-03 14:00:12 +0100

    Split db in three

---
+++ internal/letarette/db_index.go
@@ -0,0 +1,318 @@
+package letarette
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/erkkah/letarette/internal/snowball"
+	"github.com/erkkah/letarette/pkg/protocol"
+)
+
+func (db *database) getLastUpdateTime(ctx context.Context, space string) (t time.Time, err error) {
+	var timestampNanos int64
+	err = db.rdb.GetContext(ctx, &timestampNanos, "select lastUpdatedAtNanos from spaces where space = ?", space)
+	t = time.Unix(0, timestampNanos)
+	return
+}
+
+func (db *database) addDocumentUpdates(ctx context.Context, space string, docs []protocol.Document) error {
+	spaceID, err := db.getSpaceID(ctx, space)
+	if err != nil {
+		return err
+	}
+
+	tx, err := db.wdb.BeginTxx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if tx != nil {
+			tx.Rollback()
+		}
+	}()
+
+	docsStatement, err := tx.PrepareContext(ctx, `replace into docs (spaceID, docID, updatedNanos, txt, alive) values (?, ?, ?, ?, ?)`)
+	if err != nil {
+		return err
+	}
+
+	interestStatement, err := tx.PrepareContext(ctx, `update interest set state=? where spaceID=? and docID=?`)
+	if err != nil {
+		return err
+	}
+	for _, doc := range docs {
+		txt := ""
+		if doc.Alive {
+			txt = doc.Text
+		}
+		res, err := docsStatement.ExecContext(ctx, spaceID, doc.ID, doc.Updated.UnixNano(), txt, doc.Alive)
+
+		if err != nil {
+			return fmt.Errorf("Failed to update doc: %w", err)
+		}
+
+		updatedRows, _ := res.RowsAffected()
+		if updatedRows != 1 {
+			return fmt.Errorf("Failed to update index, no rows affected")
+		}
+
+		_, err = interestStatement.ExecContext(ctx, served, spaceID, doc.ID)
+
+		if err != nil {
+			return fmt.Errorf("Failed to update interest list: %w", err)
+		}
+	}
+	err = tx.Commit()
+	if err == nil {
+		tx = nil
+	}
+
+	return err
+}
+
+func (db *database) commitInterestList(ctx context.Context, space string) error {
+	tx, err := db.wdb.BeginTxx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if tx != nil {
+			tx.Rollback()
+		}
+	}()
+
+	var indexPosition struct {
+		Updated int64               `db:"updatedNanos"`
+		DocID   protocol.DocumentID `db:"docID"`
+	}
+
+	err = tx.GetContext(ctx, &indexPosition, `
+		with listState as (
+			select listCreatedAtNanos from spaces where space = ?
+		)
+		select docs.updatedNanos, docs.docID
+		from
+		interest
+		left join docs using(docID)
+		cross join listState
+		where interest.state = ? and docs.updatedNanos < listState.listCreatedAtNanos
+		order by docs.updatedNanos desc, docs.docID
+		limit 1;`, space, served)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil
+		}
+		return err
+	}
+
+	res, err := tx.ExecContext(ctx, "update spaces set lastUpdatedAtNanos = ?, lastUpdatedDocID = ? where space = ?",
+		indexPosition.Updated, indexPosition.DocID, space)
+	if err != nil {
+		return err
+	}
+
+	count, _ := res.RowsAffected()
+	if count != 1 {
+		err = fmt.Errorf("Failed to update index position for space %q", space)
+		return err
+	}
+
+	err = tx.Commit()
+	if err == nil {
+		tx = nil
+	}
+
+	return err
+}
+
+func (db *database) getInterestListState(ctx context.Context, space string) (state InterestListState, err error) {
+	err = db.rdb.GetContext(ctx, &state,
+		`select listCreatedAtNanos, lastUpdatedAtNanos, lastUpdatedDocID from spaces where space = ?`, space)
+	return
+}
+
+func (db *database) getSpaceID(ctx context.Context, space string) (int, error) {
+	var spaceID int
+	err := db.rdb.GetContext(ctx, &spaceID, `select spaceID from spaces where space = ?`, space)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			err = fmt.Errorf("No such space, %v", space)
+		}
+		return -1, err
+	}
+	return spaceID, nil
+}
+
+func (db *database) getInterestList(ctx context.Context, space string) (result []Interest, err error) {
+	spaceID, err := db.getSpaceID(ctx, space)
+	if err != nil {
+		return
+	}
+	rows, err := db.rdb.QueryxContext(ctx,
+		`
+		select docID, state from interest
+		where spaceID = ?
+		`, spaceID)
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var interest Interest
+		err = rows.StructScan(&interest)
+		if err != nil {
+			return
+		}
+		result = append(result, interest)
+	}
+	return
+}
+
+func (db *database) clearInterestList(ctx context.Context, space string) error {
+	spaceID, err := db.getSpaceID(ctx, space)
+	if err != nil {
+		return err
+	}
+	_, err = db.wdb.ExecContext(ctx, `delete from interest where spaceID = ?`, spaceID)
+	return err
+}
+
+func (db *database) resetRequested(ctx context.Context, space string) error {
+	spaceID, err := db.getSpaceID(ctx, space)
+	if err != nil {
+		return err
+	}
+	_, err = db.wdb.ExecContext(ctx, `update interest set state = ? where state = ? and spaceID = ?`,
+		pending, requested, spaceID)
+	return err
+}
+
+func (db *database) setInterestList(ctx context.Context, space string, list []protocol.DocumentID) error {
+
+	tx, err := db.wdb.BeginTxx(ctx, nil)
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+		}
+	}()
+
+	var spaceID int
+	err = tx.GetContext(ctx, &spaceID, `select spaceID from spaces where space = ?`, space)
+	if err != nil {
+		return err
+	}
+	var interestCount int
+	err = tx.GetContext(ctx, &interestCount, `select count(*) from interest where spaceID = ? and state <> ?`, spaceID, served)
+	if err != nil {
+		return err
+	}
+	if interestCount != 0 {
+		err = fmt.Errorf("Cannot overwrite active interest list")
+		return err
+	}
+	_, err = tx.ExecContext(ctx, `delete from interest where spaceID = ?`, spaceID)
+	if err != nil {
+		return err
+	}
+	st, err := tx.PreparexContext(ctx, `insert into interest (spaceID, docID, state) values(?, ?, ?)`)
+	if err != nil {
+		return err
+	}
+	defer st.Close()
+
+	for _, docID := range list {
+		_, err := st.ExecContext(ctx, spaceID, docID, pending)
+		if err != nil {
+			return err
+		}
+	}
+
+	now := time.Now().UnixNano()
+
+	_, err = tx.NamedExecContext(ctx,
+		`
+		update spaces set listCreatedAtNanos = :now
+		where spaceID = :spaceID
+		`,
+
+		map[string]interface{}{
+			"spaceID": spaceID,
+			"now":     now,
+		})
+	if err != nil {
+		return err
+	}
+	err = tx.Commit()
+	return err
+}
+
+func (db *database) setInterestState(ctx context.Context, space string, docID protocol.DocumentID, state InterestState) error {
+	spaceID, err := db.getSpaceID(ctx, space)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.wdb.ExecContext(ctx, "update interest set state = ? where spaceID=? and docID=?", state, spaceID, docID)
+	return err
+}
+
+func (db *database) getStemmerState() (snowball.Settings, time.Time, error) {
+	query := `
+	select
+	languages,
+	removeDiacritics as removediacritics,
+	tokenCharacters as tokencharacters,
+	separators,
+	updated
+	from stemmerstate
+	`
+	var state struct {
+		Languages string
+		Updated   time.Time
+		snowball.Settings
+	}
+	err := db.rdb.Get(&state, query)
+	if len(state.Languages) == 0 {
+		state.Stemmers = []string{}
+	} else {
+		state.Stemmers = strings.Split(state.Languages, ",")
+	}
+	return state.Settings, state.Updated, err
+}
+
+func (db *database) setStemmerState(state snowball.Settings) error {
+	_, _, err := db.getStemmerState()
+
+	if err == sql.ErrNoRows {
+		insert := `
+		insert into stemmerstate (languages, removeDiacritics, tokenCharacters, separators)
+		values ("", "false", "", "");
+		`
+		result, err := db.wdb.Exec(insert)
+		if err != nil {
+			return err
+		}
+		if rows, err := result.RowsAffected(); err != nil || rows != 1 {
+			return fmt.Errorf("Failed to insert default state")
+		}
+	}
+	query := `
+	update stemmerstate
+	set languages = ?, removeDiacritics = ?, tokenCharacters = ?, separators = ?
+	`
+
+	languages := strings.Join(state.Stemmers, ",")
+	_, err = db.wdb.Exec(query,
+		languages,
+		state.RemoveDiacritics,
+		state.TokenCharacters,
+		state.Separators,
+	)
+	return err
+}
