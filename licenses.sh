21210d19801bd4d4bf197babaa7a49a7e32157a0
Erik Agsjö <erkkah@users.noreply.github.com>
2019-12-08 23:37:07 +0100

    Releasescripts (#6)

---
+++ licenses.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#
+# Gather licenses for binary releases
+#
+
+devdeps="
+gotest.tools
+"
+
+manualdeps="
+github.com/snowballstem/snowball:internal/snowball/snowball/COPYING
+"
+
+TARGET=licenses
+mkdir -p $TARGET
+NOTICE=$TARGET/NOTICE
+cp LICENSE $TARGET
+
+echo "
+Letarette is licensed under the Apache v2.0 License.
+Letarette uses the following fine packages:
+---
+" > $NOTICE
+
+while read -r dep; do
+    if [ "$dep" == "" ]; then
+        continue
+    fi
+    IFS=':' read -r -a split <<< "$dep"
+    pkg="${split[0]}"
+    lic="${split[1]}"
+    echo $pkg >> $NOTICE
+    mkdir -p "$TARGET/$pkg"
+    cp "$lic" "$TARGET/$pkg"
+done <<< "$manualdeps"
+
+go mod vendor
+licenses=`find vendor -name LICENSE | sed -e 's/vendor\///'`
+
+for lic in $licenses; do
+    pkg=`echo $lic | sed -e 's/\/LICENSE//'`
+
+    for devdep in $devdeps; do
+        if grep -q "^$devdep" <<< "$pkg"; then
+            continue 2
+        fi
+    done
+
+    if go mod why -m $pkg | grep -q "does not need"; then
+        continue
+    fi
+
+    echo $pkg >> $NOTICE
+    mkdir -p "$TARGET/$pkg"
+    cp "vendor/$lic" "$TARGET/$pkg"
+done
+
+echo "
+---
+
+Find the license for each package in the corresponding subdirectory.
+" >> $NOTICE
