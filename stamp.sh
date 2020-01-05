0f7bd390b09234a888c4d8ae62c710d05ac0aa54
Erik Agsjö <erik.agsjo@gmail.com>
2020-01-05 11:12:11 +0100

    Version stamp fix

--- stamp.sh
+++ stamp.sh
@@ -2,14 +2,17 @@ package=${1:?Expected package argument}
 
 if output=$(git status --porcelain) && [ -z "$output" ]; then
     # clean
+    echo clean repo >&2
     sha=$(git rev-parse HEAD)
     rev=${sha:0:7}
 else
     # dirty
+    echo dirty repo >&2
+    echo "$output" >&2
     rev="dev"
 fi
 
-if tag=$(git tag) && [ -z "$tag"]; then
+if tag=$(git tag --contains) && [ -z "$tag" ]; then
     tag=$(date +%F)
 fi
 
