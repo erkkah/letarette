061a4da7888d38e06e0b25f6a374b8016609a4b7
Erik Agsjö <erkkah@users.noreply.github.com>
2020-01-05 14:06:05 +0100

    Version stamping + Docker fixes (#22)
    * Version stamp fix
    
    * Silly me
    
    * Status check
    
    * Init submodule before cache
    
    * Update submodule before cache
    
    * Print debug
    
    * Split debug print
    
    * It's a bash script!
    
    * Stamp + cache fix
    
    * Push to docker hub
    
    * Updated docker setup
    
    * Test-build docker image

--- stamp.sh
+++ stamp.sh
@@ -1,15 +1,21 @@
+#!/bin/bash
+
 package=${1:?Expected package argument}
 
 if output=$(git status --porcelain) && [ -z "$output" ]; then
     # clean
+    echo clean repo >&2
     sha=$(git rev-parse HEAD)
     rev=${sha:0:7}
+    echo rev:$rev >&2
 else
     # dirty
+    echo dirty repo >&2
+    echo "$output" >&2
     rev="dev"
 fi
 
-if tag=$(git tag) && [ -z "$tag"]; then
+if tag=$(git tag --contains) && [ -z "$tag" ]; then
     tag=$(date +%F)
 fi
 
