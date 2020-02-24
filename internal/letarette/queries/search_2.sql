12d74f4d8e6512cef4948975573c1e92dd18e318
Erik Agsjö <erik.agsjo@gmail.com>
2020-02-24 18:58:25 +0100

    Make search queries handle uncompressed text

--- internal/letarette/queries/search_2.sql
+++ internal/letarette/queries/search_2.sql
@@ -36,7 +36,11 @@ select
         gettokens(fts,
             case matchColumn
                 when 0 then docs.title
-                when 1 then uncompress(docs.txt)
+                when 1 then case
+                    when iscompressed(docs.txt) then uncompress(docs.txt)
+                    else docs.txt
+                end
+
             end,
             max(matchOffset-1, 0), 10),
         X'0A', " "
