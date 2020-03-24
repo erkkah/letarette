dbb86474f20fff806d11277f9892e4e49dc0330e
Erik Agsjö <erik.agsjo@gmail.com>
2020-03-24 23:55:09 +0100

    Load + dump synonyms (lrcli)

--- internal/letarette/db_stopwords.go
+++ internal/letarette/db_stopwords.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Erik Agsjö
+// Copyright 2020 Erik Agsjö
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
