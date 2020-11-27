531731962b971e7b623b5844f7cf1ee170a0e20b
Erik Agsjö <erik.agsjo@gmail.com>
2020-11-27 14:10:52 +0100

    Benchmark stuff

---
+++ bench/README.md
@@ -0,0 +1,1 @@
+# Letarette benchmarks
---
+++ bench/bulk.sh
@@ -0,0 +1,9 @@
+compose="docker-compose -f docker-compose-bulk.yml"
+$compose down > /dev/null 2>&1
+docker volume rm bench_dbvolume > /dev/null 2>&1
+
+$compose up | grep Loaded | tee bench.log
+
+duration=`grep -m1 "10000\sdocuments" bench.log | awk '{print $7}' | cut -d',' -f1`
+
+echo Load time: $duration
