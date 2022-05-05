fa71cba7422ad00378933358468f01813a446deb
Erik Agsjö <erik.agsjo@gmail.com>
2022-05-05 22:57:54 +0200

    Always build in benchmarks

--- bench/load.sh
+++ bench/load.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker-compose down > /dev/null 2>&1
+docker compose down > /dev/null 2>&1
 docker volume rm bench_dbvolume > /dev/null 2>&1
 
 OS=`uname -s`
@@ -8,8 +8,8 @@ if [ $OS == Darwin ]; then
     OPT=--line-buffered
 fi
 
-docker-compose -f docker-compose.yml up -d
-docker-compose logs -f | grep $OPT monitor | tee bench.log | \
-    (grep $OPT -m1 '10000 docs' && docker-compose down)
+docker compose -f docker-compose.yml up --build -d
+docker compose logs -f | grep $OPT monitor | tee bench.log | \
+    (grep $OPT -m1 '10000 docs' && docker compose down)
 
 docker run --rm -v ${PWD}:/bench -w /bench alpine ./load-report.sh
