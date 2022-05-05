a1838f0b60a22654e4b08c6be606bbba44dd2a9a
Erik Agsjö <erik.agsjo@gmail.com>
2022-05-05 22:57:54 +0200

    Always build in benchmarks

--- bench/bulk.sh
+++ bench/bulk.sh
@@ -1,8 +1,8 @@
-compose="docker-compose -f docker-compose-bulk.yml"
+compose="docker compose -f docker-compose-bulk.yml"
 $compose down > /dev/null 2>&1
 docker volume rm bench_dbvolume > /dev/null 2>&1
 
-$compose up | grep Loaded | tee bench.log
+$compose up --build | grep Loaded | tee bench.log
 
 duration=`grep -m1 "10000\sdocuments" bench.log | awk '{print $7}' | cut -d',' -f1`
 
